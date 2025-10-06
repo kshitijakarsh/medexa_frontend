8e7ddc823b503f7f3405ac71f5519713853cab84
Manav Gupta <manavgupta14032003@gmail.com>
2025-10-06 21:21:56 +0530

    feat: update ESLint configuration, styles, and add admin CSS

--- apps/admin/eslint.config.js
+++ apps/admin/eslint.config.js
@@ -1,4 +1,13 @@
 import { nextJsConfig } from "@workspace/eslint-config/next-js"
 
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  ...nextJsConfig,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
+]
