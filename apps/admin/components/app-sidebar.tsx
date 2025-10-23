63a32ffa72ecbe17a8a49f821477ad76d5c3e372
Manav Gupta <manavgupta14032003@gmail.com>
2025-10-23 21:29:20 +0530

    feat(onboarding): add 'Skip' button to LicenceHistoryStepForm, ModuleStepForm, and PaymentStepForm for improved navigation

--- apps/admin/components/app-sidebar.tsx
+++ apps/admin/components/app-sidebar.tsx
@@ -53,7 +53,14 @@ export function AppSidebar({ isStandalonePage, dict }: AppSidebarProps) {
     },
     {
       title: dict.nav.hospitals,
-      url: ["/hospitals", "/create-hospital", "/onboarding/modules", "/onboarding/payment", "/onboarding/licence-history", "/onboarding/regulatory-docs"], // multiple URLs
+      url: [
+        "/hospitals",
+        "/create-hospital",
+        "/onboarding/modules",
+        "/onboarding/payment",
+        "/onboarding/licence-history",
+        "/onboarding/regulatory-docs",
+      ], // multiple URLs
       icon: Hospital,
     },
     {
@@ -71,7 +78,6 @@ export function AppSidebar({ isStandalonePage, dict }: AppSidebarProps) {
       url: ["/monitoring"],
       icon: Monitor,
     },
-
   ]
 
   // const isActive = (url: string) => {
@@ -82,35 +88,39 @@ export function AppSidebar({ isStandalonePage, dict }: AppSidebarProps) {
   //     : pathWithoutLocale.startsWith(url)
   // }
   const isActive = (urls: string[] | string) => {
-    const pathWithoutLocale = pathname.replace(/^\/[a-z]{2}/, "");
+    const pathWithoutLocale = pathname.replace(/^\/[a-z]{2}/, "")
 
     if (Array.isArray(urls)) {
       return urls.some((url) =>
-        url === "/" ? pathWithoutLocale === "" || pathWithoutLocale === "/" : pathWithoutLocale.startsWith(url)
-      );
+        url === "/"
+          ? pathWithoutLocale === "" || pathWithoutLocale === "/"
+          : pathWithoutLocale.startsWith(url)
+      )
     }
 
-    return urls === "/" ? pathWithoutLocale === "" || pathWithoutLocale === "/" : pathWithoutLocale.startsWith(urls);
-  };
-
+    return urls === "/"
+      ? pathWithoutLocale === "" || pathWithoutLocale === "/"
+      : pathWithoutLocale.startsWith(urls)
+  }
 
   const sidebarState = useSidebar().state
 
   const handleLogout = () => {
-    logoutCognitoUser();
-    window.location.href = "/login"; // full page reload
-  };
+    logoutCognitoUser()
+    window.location.href = "/login" // full page reload
+  }
 
   const groups = [
     items.slice(0, 1), // first group
     items.slice(1, 3), // second group
     items.slice(3, 5), // second group
-  ];
-
-
+  ]
 
   return (
-    <Sidebar collapsible={isStandalonePage ? "icon" : "offcanvas"} className="[&[data-state=collapsed]]:!w-[7rem]">
+    <Sidebar
+      collapsible={isStandalonePage ? "icon" : "offcanvas"}
+      className="[&[data-state=collapsed]]:!w-[7rem]"
+    >
       <SidebarHeader className="flex items-center justify-center py-4">
         <div className="relative w-full flex items-center justify-center overflow-hidden">
           <Image
@@ -118,10 +128,11 @@ export function AppSidebar({ isStandalonePage, dict }: AppSidebarProps) {
             alt="MedExe"
             width={130}
             height={50}
-            className={`transition-all duration-300 ease-in-out ${sidebarState === "expanded"
-              ? "opacity-100 scale-100"
-              : "opacity-0 scale-75 absolute"
-              }`}
+            className={`transition-all duration-300 ease-in-out ${
+              sidebarState === "expanded"
+                ? "opacity-100 scale-100"
+                : "opacity-0 scale-75 absolute"
+            }`}
             priority
           />
           <Image
@@ -129,10 +140,11 @@ export function AppSidebar({ isStandalonePage, dict }: AppSidebarProps) {
             alt="MedExe"
             width={30}
             height={25}
-            className={`transition-all duration-300 ease-in-out ${sidebarState === "collapsed"
-              ? "opacity-100 scale-100"
-              : "opacity-0 scale-75 absolute"
-              }`}
+            className={`transition-all duration-300 ease-in-out ${
+              sidebarState === "collapsed"
+                ? "opacity-100 scale-100"
+                : "opacity-0 scale-75 absolute"
+            }`}
             priority
           />
         </div>
@@ -159,18 +171,32 @@ export function AppSidebar({ isStandalonePage, dict }: AppSidebarProps) {
       <SidebarContent className="gap-0">
         {groups.map((group, index) => (
           <SidebarGroup key={index} className="mb-0">
-            <SidebarGroupLabel>{index === 0 ? dict.nav.application : index === 1 ? dict.nav.management : dict.nav.monitor}</SidebarGroupLabel>
+            <SidebarGroupLabel>
+              {index === 0
+                ? dict.nav.application
+                : index === 1
+                  ? dict.nav.management
+                  : dict.nav.monitor}
+            </SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
-                {group.map(item => (
-                  <SidebarMenuItem key={item.title} className={`bg-[linear-gradient(90deg,_#07235B_0%,_#001A4D_100%)] rounded  ${sidebarState === "collapsed" && "ms-2"}`}>
-                    <SidebarMenuButton asChild isActive={isActive(item.url)} className="text-sm p-3">
+                {group.map((item) => (
+                  <SidebarMenuItem
+                    key={item.title}
+                    className={`bg-[linear-gradient(90deg,_#07235B_0%,_#001A4D_100%)] rounded  ${sidebarState === "collapsed" && "ms-2"}`}
+                  >
+                    <SidebarMenuButton
+                      asChild
+                      isActive={isActive(item.url)}
+                      className="text-sm p-3"
+                    >
                       <LocaleLink href={item.url[0] /* main URL for link */}>
                         <item.icon
-                          className={`transition-transform duration-300 ${sidebarState === "collapsed" ? "w-7 h-7 hover:scale-110" : "w-5 h-5"
-                            }`}
+                          className={`transition-transform duration-300 ${
+                            sidebarState === "collapsed" ? "w-7 h-7" : "w-5 h-5"
+                          }`}
                         />
-                        <span>{item.title}</span>
+                        <span className="text-base">{item.title}</span>
                       </LocaleLink>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
@@ -182,7 +208,9 @@ export function AppSidebar({ isStandalonePage, dict }: AppSidebarProps) {
       </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
-          <SidebarMenuItem className={`bg-[linear-gradient(90deg,_#07235B_0%,_#001A4D_100%)] rounded  ${sidebarState === "collapsed" && "ms-2"}`}>
+          <SidebarMenuItem
+            className={`bg-[linear-gradient(90deg,_#07235B_0%,_#001A4D_100%)] rounded  ${sidebarState === "collapsed" && "ms-2"}`}
+          >
             {/* <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton>
@@ -205,7 +233,10 @@ export function AppSidebar({ isStandalonePage, dict }: AppSidebarProps) {
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu> */}
-            <SidebarMenuButton onClick={handleLogout} className="cursor-pointer">
+            <SidebarMenuButton
+              onClick={handleLogout}
+              className="cursor-pointer"
+            >
               <LogOut className="h-4 w-4" />
               Sign out
             </SidebarMenuButton>
